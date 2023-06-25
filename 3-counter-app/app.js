f0f784cb172f2702e24973c26c4a53b176cc6cfe
Gurkan Yakar <data.yakar@gmail.com>
2023-06-26 01:37:03 +0300

    reactivity: computed

---
+++ 3-counter-app/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      counter2: 0,
+    };
+  },
+  methods: {
+    // increaseCounter(){
+    //     this.counter +=1;
+    // },
+    // decreaseCounter(){
+    //     this.counter >= 1 ? this.counter-=1: this.counter =0;
+    // }
+    // getCounterResult(){
+    //     console.log("counter 1 work");
+    //     return this.counter > 5 ? "bigger than 5" : "less than 5";
+    // },
+    // getCounterResult2(){
+    //     console.log("counter 2 work");
+    //     return this.counter2 > 5 ? "bigger than 5" : "less than 5";
+    // }
+  },
+  computed: {
+    getCounterResult() {
+      console.log("counter 1 work");
+      return this.counter > 5 ? "bigger than 5" : "less than 5";
+    },
+    getCounterResult2() {
+      console.log("counter 2 work");
+      return this.counter2 > 5 ? "bigger than 5" : "less than 5";
+    },
+  },
+}).mount("#app");
